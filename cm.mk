8c500683451d1a6b5f6f89e2f5018c9ed16ea7ba
pj <moonsin.ph@hotmail.com>
2012-09-29 11:28:48 +0700

    Changed to cm10

--- cm.mk
+++ cm.mk
@@ -2,15 +2,14 @@
 PRODUCT_RELEASE_NAME := A500
 
 # Inherit some common CM stuff.
-#$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/acer/a500/a500.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE       := a500
-PRODUCT_NAME         := aokp_a500
+PRODUCT_NAME         := cm_a500
 PRODUCT_BRAND        := Acer
 PRODUCT_MODEL        := A500
 PRODUCT_MANUFACTURER := Acer
@@ -21,5 +20,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="acer/a500_ww_gen1/picasso:4.0.3/IML74K/1329824557:user/release-keys" \
     PRIVATE_BUILD_DESC="a500_ww_gen1-user 4.0.3 IML74K 1329824557 release-keys"
 
-PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation.zip
